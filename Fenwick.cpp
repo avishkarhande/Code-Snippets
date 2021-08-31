50e018a76a89ed380484c91909c4faad953100cb
unknown <avishkarhande@gmail.com>
2021-08-31 20:54:57 +0530

    initial commit

---
+++ Fenwick.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+struct FenwickTreeOneBasedIndexing {
+    vector<ll> bit;  // binary indexed tree
+    ll n;
+
+    FenwickTreeOneBasedIndexing(ll n) {
+        this->n = n + 1;
+        bit.assign(n + 1, 0);
+    }
+
+    FenwickTreeOneBasedIndexing(vector<ll> a)
+        : FenwickTreeOneBasedIndexing(a.size()) {
+        for (size_t i = 0; i < a.size(); i++)
+            add(i, a[i]);
+    }
+
+    ll sum(ll idx) {
+        ll ret = 0;
+        for (++idx; idx > 0; idx -= idx & -idx)
+            ret += bit[idx];
+        return ret;
+    }
+
+    ll sum(ll l, ll r) {
+        return sum(r) - sum(l - 1);
+    }
+
+    void add(ll idx, ll delta) {
+        for (++idx; idx < n; idx += idx & -idx)
+            bit[idx] += delta;
+    }
+};
+
+int main()
+{
+
+}
